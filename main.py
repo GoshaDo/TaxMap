c4b5f97d271abd4b6a212134e7db9c81ea341c21
george.dozoretz <goshatoo@gmail.com>
2021-10-11 19:06:32 +0300

    Init

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import requests
+from pathlib import Path
+from os import listdir
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+URL = 'https://www.gov.il/BlobFolder/generalpage/' \
+      'income-tax-monthly-deductions-booklet/he/' \
+      'itc_itc_necuyim2021-1.pdf#page=18'
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if 'metadata.pdf' not in listdir():
+        page = requests.get(URL)
+        filename = Path('metadata.pdf')
+        filename.write_bytes(page.content)
